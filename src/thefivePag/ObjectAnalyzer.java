1ae3ff79f2bd7803c6274dfb694be61b85814344
liulu961126 <nightmorepop@qq.com>
2019-06-10 23:03:12 +0800

    第5次提交

--- src/thefivePag/ObjectAnalyzer.java
+++ src/thefivePag/ObjectAnalyzer.java
@@ -12,11 +12,16 @@ import java.lang.reflect.Field;
 @SuppressWarnings("all")
 public class ObjectAnalyzer {
     public static void main(String[] args) {
+
         try {
+            //创建一个对象
             Employee employee = new Employee("Harry Hacke", 3520, 10, 11, 1);
-            Class cl = employee.getClass();
-            Field f = cl.getDeclaredField("name");
-            Object v =f.get(employee);
+            Class cl = employee.getClass();//得到运行时类
+            Field f = cl.getDeclaredField("salary");//得到一个包含指定域的对象
+            //设置安全级别
+            f.setAccessible(true);
+            Object v = f.getDouble(employee);//得到这个指定域对象的当前值,受控于访问权限
+            System.out.println(v.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
